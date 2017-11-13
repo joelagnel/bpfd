2474b17676b72aa46d8816db3edf999846492a69
root <joelaf@google.com>
2017-11-12 16:58:49 -0800

    bpfd: add library for base64
    Signed-off-by: root <joelaf@google.com>

--- Makefile
+++ Makefile
@@ -13,8 +13,9 @@ libbpf.o: $(LIBBPF_SRC)
 libbpf_bpfd.so: libbpf.o
 	$(CC) $(CFLAGS) -shared -o $@ $^
 
-bpfd: $(BPFD_SRC) libbpf_bpfd.so
-	$(CC) $(CFLAGS) -lbpf_bpfd -o $@ $(BPFD_SRC)
+bpfd: $(BPFD_SRC) base64.c libbpf_bpfd.so
+	$(CC) $(CFLAGS) -c -o base64.o base64.c
+	$(CC) $(CFLAGS) -lbpf_bpfd base64.o -o $@ $(BPFD_SRC)
 
 all: bpfd
 
--- bpfd.c
+++ bpfd.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/stat.h>
 
 #define LINEBUF_SIZE  100
 #define LINE_TOKENS   10
+
+int base64_encode(unsigned char *source, size_t sourcelen, char *target, size_t targetlen);
+size_t base64_decode( char *source, unsigned char *target, size_t targetlen);
 
 int read_avail_filter(char *tracefs) {
 	char tracef[100], ch;
@@ -35,11 +40,58 @@ int read_avail_filter(char *tracefs) {
 
 }
 
+void test_base64(char *file) {
+	struct stat st;
+	char *fileout, *encoded, *filebuf;
+	size_t size;
+	int ret;
+	FILE *fp;
+	char *target;
+
+	stat(file, &st);
+	size = st.st_size;
+
+	fileout = (char *)malloc(strlen(file) + 1 + 4);
+	fileout[0] = 0;
+	strcat(fileout, file);
+	strcat(fileout, ".b64dec");
+
+	printf("Encoding and then decoding %s into %s, filesize is %d\n", file, fileout, size);
+
+	encoded = (char *)malloc((size * 4) + 1);
+	encoded[(size * 4)] = 0;
+
+	filebuf = (char *)malloc(size);
+	fp = fopen(file, "rb");
+	fread(filebuf, size, 1, fp);
+
+	ret = base64_encode(filebuf, size, encoded, size*4);
+
+	printf("encoded len: %d\n", strlen(encoded));
+
+	printf("encoded stat: %s\n", encoded);
+
+	target = (char *)malloc(size);
+
+	ret = base64_decode(encoded, target, size);
+
+	fp = fopen(fileout, "wb");
+
+	printf("fp=%p ret=%d fileout=%s\n", (void *)fp, ret, fileout);
+
+	fwrite(target, size, 1, fp);
+
+	fclose(fp);
+}
+
 int main(int argc, char **argv)
 {
 	char line_buf[LINEBUF_SIZE];
 	char *cmd, *lineptr, *argstr;
 	int len, fd;
+
+	if (argc == 2 && !strcmp(argv[1], "base64"))
+		test_base64("bpfd.c");
 
 	while (fgets(line_buf, LINEBUF_SIZE, stdin)) {
 		int fd;
