d984e72eb9619e137144f185c73d1424da0db8d4
root <joelaf@google.com>
2017-11-14 13:15:01 -0800

    add license
    Signed-off-by: root <joelaf@google.com>

--- bpfd.c
+++ bpfd.c
@@ -1,3 +1,13 @@
+/*
+ * BPFd (Berkeley Packet Filter daemon)
+ *
+ * Copyright (C) 2017 Joel Fernandes <agnel.joel@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
