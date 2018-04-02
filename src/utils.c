9c07ed17babd2b2e9d7a1ae7250cfe6202aaf50c
Jazel Canseco <jcanseco@google.com>
2018-04-02 16:17:40 -0700

    Add command to read the "comm" file for a given pid
    Signed-off-by: Jazel Canseco <jcanseco@google.com>

--- src/utils.c
+++ src/utils.c
@@ -59,6 +59,14 @@ int cat_tracefs_file(char *tracefs, char *fn) {
 	return cat_file(tracef);
 }
 
+int cat_comm_file(int pid) {
+	char commf[100];
+
+	snprintf(commf, 100, "/proc/%d/comm", pid);
+
+	return cat_file(commf);
+}
+
 int cat_dir(char *path, int dirs_only)
 {
 	DIR *dp;
