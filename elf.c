632ba7df0febe4fda1ffc0c6bd63c6afb46c1091
Joel Fernandes (Google) <joelaf@google.com>
2018-08-16 13:50:28 -0700

    Since we're only supporting 64-bit elf, change naming
    Signed-off-by: Joel Fernandes (Google) <joelaf@google.com>

--- elf.c
+++ elf.c
@@ -13,7 +13,7 @@
 	perror(line); exit(0);								\
 }
 
-Elf64_Ehdr read_elf_header(char *elfpath)
+Elf64_Ehdr read_elf64_header(char *elfpath)
 {
 	Elf64_Ehdr eh;
 	FILE *elf_file;
@@ -26,7 +26,7 @@ Elf64_Ehdr read_elf_header(char *elfpath)
 }
 
 /* Reads all section header tables into an Shdr array */
-Elf64_Shdr *read_section_headers_all(char *elfpath, int *entries)
+Elf64_Shdr *read_section64_headers_all(char *elfpath, int *entries)
 {
 	Elf64_Ehdr eh;
 	Elf64_Shdr *sh_table;
@@ -35,7 +35,7 @@ Elf64_Shdr *read_section_headers_all(char *elfpath, int *entries)
 	elf_file = fopen(elfpath, "r");
 	assert(!!elf_file);
 
-	eh = read_elf_header(elfpath);
+	eh = read_elf64_header(elfpath);
 
 	/* Read offset of shdr table */
 	assert(fseek(elf_file, eh.e_shoff, SEEK_SET) == 0);
@@ -53,7 +53,7 @@ Elf64_Shdr *read_section_headers_all(char *elfpath, int *entries)
 }
 
 /* Read a section by its index - for ex to get sec hdr strtab blob */
-void *read_section_by_id(char *elfpath, int id, int *bytes)
+void *read_section64_by_id(char *elfpath, int id, int *bytes)
 {
 	Elf64_Shdr *sh_table;
 	Elf64_Off shoff;
@@ -64,7 +64,7 @@ void *read_section_by_id(char *elfpath, int id, int *bytes)
 	elf_file = fopen(elfpath, "r");
 	assert(!!elf_file);
 
-	sh_table = read_section_headers_all(elfpath, &entries);
+	sh_table = read_section64_headers_all(elfpath, &entries);
 
 	section = (char *)malloc(sh_table[id].sh_size);
 	assert(fseek(elf_file, sh_table[id].sh_offset, SEEK_SET) == 0);
@@ -78,7 +78,7 @@ void *read_section_by_id(char *elfpath, int id, int *bytes)
 }
 
 /* Read whole section header string table */
-char *read_section_header_strtab(char *elfpath, int *bytes)
+char *read_section64_header_strtab(char *elfpath, int *bytes)
 {
 	Elf64_Ehdr eh;
 	FILE *elf_file;
@@ -87,15 +87,15 @@ char *read_section_header_strtab(char *elfpath, int *bytes)
 	elf_file = fopen(elfpath, "r");
 	assert(!!elf_file);
 
-	eh = read_elf_header(elfpath);
-	strtab = (char *)read_section_by_id(elfpath, eh.e_shstrndx, bytes);
+	eh = read_elf64_header(elfpath);
+	strtab = (char *)read_section64_by_id(elfpath, eh.e_shstrndx, bytes);
 
 	fclose(elf_file);
 	return strtab;
 }
 
 /* Reads a full section by name - example to get the GPL license */
-void *read_section_by_name(char *name, char *elfpath, int *bytes)
+void *read_section64_by_name(char *name, char *elfpath, int *bytes)
 {
 	char *sec_strtab;
 	char *data = NULL;
@@ -105,8 +105,8 @@ void *read_section_by_name(char *name, char *elfpath, int *bytes)
 
 	elf_file = fopen(elfpath, "r");
 	assert(!!elf_file);
-	sh_table = read_section_headers_all(elfpath, &n_sh_table);
-	sec_strtab = read_section_header_strtab(elfpath, bytes);
+	sh_table = read_section64_headers_all(elfpath, &n_sh_table);
+	sec_strtab = read_section64_header_strtab(elfpath, bytes);
 
 	for(int i = 0; i < n_sh_table; i++) {
 		char *secname = sec_strtab + sh_table[i].sh_name;
@@ -135,7 +135,7 @@ int main()
 	char elfpath[] = "tracex2_kern.o";
 	int bytes;
 
-	license = read_section_by_name("license", elfpath, &bytes);
+	license = read_section64_by_name("license", elfpath, &bytes);
 	printf("License: %s\n", license);
 	return 0;
 }
