1fc639a5375062b46e0d3183ac2b368a4f1030d7
Joel Fernandes <joelaf@google.com>
2018-09-28 13:14:48 -0700

    Updates maps code
    Signed-off-by: Joel Fernandes <joelaf@google.com>

--- android/bpfmap.h
+++ android/bpfmap.h
@@ -21,6 +21,11 @@
 #include <functional>
 #include "libbpf.h"
 
+// Prototypes of map filter functions
+#define filter_key_t std::function<int(const Key &key, const BpfMap<Key, Value> &map)>
+#define filter_key_val_t \
+ std::function<int(const Key &key, const Value &value, const BpfMap<Key, Value> &map)>
+
 #if 0
 #include <utils/Log.h>
 #include "bpf/BpfUtils.h"
@@ -30,163 +35,159 @@
 
 #define ALOGE(...)
 
-namespace android {
-namespace bpfutils {
-		// This is a class wrapper for eBPF maps. The eBPF map is a special in-kernel
-		// data structure that stores data in <Key, Value> pairs. It can be read/write
-		// from userspace by passing syscalls with the map file descriptor. This class
-		// is used to generalize the procedure of interacting with eBPF maps and hide
-		// the implementation detail from other process. Besides the basic syscalls
-		// wrapper, it also provides some useful helper functions as well as an iterator
-		// nested class to iterate the map more easily.
-		//
-		// NOTE: A kernel eBPF map may be accessed by both kernel and userspace
-		// processes at the same time. Or if the map is pinned as a virtual file, it can
-		// be obtained by multiple eBPF map class object and and accessed concurrently.
-		// Though the map class object and the underlying kernel map are thread safe, it
-		// is not safe to iterate over a map while another thread or process is deleting
-		// from it. In this case the iteration can return duplicate entries.
-		template <class Key, class Value>
-				class BpfMap {
-						public:
-								BpfMap<Key, Value>() : mMapFd(-1){};
-								BpfMap<Key, Value>(int fd) : mMapFd(fd){};
-								BpfMap<Key, Value>(std::string pin_path) {
-										int ret = bpf_obj_get(pin_path.c_str());
-										if (ret < 0)
-												throw new runtime_error("Cannot open pinned map  location.");
-										mPinnedPath = pin_path;
-										mMapFd = ret;
-								}
+// This is a class wrapper for eBPF maps. The eBPF map is a special in-kernel
+// data structure that stores data in <Key, Value> pairs. It can be read/write
+// from userspace by passing syscalls with the map file descriptor. This class
+// is used to generalize the procedure of interacting with eBPF maps and hide
+// the implementation detail from other process. Besides the basic syscalls
+// wrapper, it also provides some useful helper functions as well as an iterator
+// nested class to iterate the map more easily.
+//
+// NOTE: A kernel eBPF map may be accessed by both kernel and userspace
+// processes at the same time. Or if the map is pinned as a virtual file, it can
+// be obtained by multiple eBPF map class object and and accessed concurrently.
+// Though the map class object and the underlying kernel map are thread safe, it
+// is not safe to iterate over a map while another thread or process is deleting
+// from it. In this case the iteration can return duplicate entries.
+template <class Key, class Value>
+class BpfMap {
+    public:
+        BpfMap<Key, Value>() : mMapFd(-1){};
+        BpfMap<Key, Value>(int fd) : mMapFd(fd){};
+        BpfMap<Key, Value>(std::string pin_path) {
+            int ret = bpf_obj_get(pin_path.c_str());
+            if (ret < 0)
+                throw new std::runtime_error("Cannot open pinned map location.");
 
-								int getMap() const { return mMapFd; };
-								bool isValid() const { return mMapFd != -1; }
-								const std::string getPinnedPath() const { return mPinnedPath; };
+            mPinnedPath = pin_path;
+            mMapFd = ret;
+        }
 
-								// Return a pair containing Key and status
-								std::pair<Key, int> getFirstKey() const {
-										Key firstKey;
-										int ret = bpf_get_first_key(mMapFd, &firstKey, sizeof(Key));
-										if (ret < 0) { ret = -errno; }
+        int getMap() const { return mMapFd; };
+        bool isValid() const { return mMapFd != -1; }
+        const std::string getPinnedPath() const { return mPinnedPath; };
 
-										return std::make_pair(firstKey, ret);
-								}
+        // Return a pair containing Key and status
+        std::pair<Key, int> getFirstKey() const {
+            Key firstKey;
+            int ret = bpf_get_first_key(mMapFd, &firstKey, sizeof(Key));
+            if (ret < 0) { ret = -errno; }
 
-								// Return a pair containing Key and status
-								std::pair<Key, int> getNextKey(const Key &key) const {
-										Key nextKey;
-										int ret = bpf_get_next_key(mMapFd, &key);
-										if (ret < 0) { ret = -errno; }
+            return std::make_pair(firstKey, ret);
+        }
 
-										return std::make_pair(nextKey, ret);
-								}
+        // Return a pair containing Key and status
+        std::pair<Key, int> getNextKey(const Key &key) const {
+            Key nextKey;
+            int ret = bpf_get_next_key(mMapFd, &key);
+            if (ret < 0) { ret = -errno; }
 
-								// Return status
-								int update(const Key& key, const Value& value, uint64_t flags) const {
-										int ret = bpf_update_elem(mMapFd, &key, &value, flags);
-										if (ret < 0) return -errno;
-										return ret;
-								}
+            return std::make_pair(nextKey, ret);
+        }
 
-								// Return a pair containing Value and status
-								std::pair<Value, int> lookup(const Key& key) const {
-										Value value;
-										int ret = bpf_lookup_elem(mMapFd, &key, &value);
-										if (ret < 0) { ret = -errno; }
+        // Return status
+        int update(const Key& key, const Value& value, uint64_t flags) const {
+            int ret = bpf_update_elem(mMapFd, &key, &value, flags);
+            if (ret < 0) return -errno;
+            return ret;
+        }
 
-										return std::make_pair(value, ret);
-								}
+        // Return a pair containing Value and status
+        std::pair<Value, int> lookup(const Key& key) const {
+            Value value;
+            int ret = bpf_lookup_elem(mMapFd, &key, &value);
+            if (ret < 0) { ret = -errno; }
 
-								// Return status
-								int delete_elem(const Key& key) const {
-										int ret = bpf_delete_elem(mMapFd, &key);
-										if (ret < 0) return -errno;
-										return ret;
-								}
+            return std::make_pair(value, ret);
+        }
 
-								void reset(int fd = -1) {
-										mMapFd = -1;
-										mPinnedPath.clear();
-								}
+        // Return status
+        int delete_elem(const Key& key) const {
+            int ret = bpf_delete_elem(mMapFd, &key);
+            if (ret < 0) return -errno;
+            return ret;
+        }
 
-								// It is only safe to call this method if it is guaranteed that nothing will concurrently
-								// iterate over the map in any process.
-								int clear() {
-										const auto deleteAllEntries = [](const Key& key, BpfMap<Key, Value>& map) {
-												int res = map.delete_elem(key);
-												if (res < 0 && res != -ENOENT)
-														ALOGE("Failed to delete map data %s\n", strerror(res.code()));
+        void reset(int fd = -1) {
+            mMapFd = -1;
+            mPinnedPath.clear();
+        }
 
-												return res;
-										};
-										iterate(deleteAllEntries);
-										return 0;
-								}
+        // It is only safe to call this method if guaranteed nothing
+        // will concurrently iterate over the map in any process.
+        int clear() {
+            const auto deleteAllEntries = [](const Key& key, BpfMap<Key, Value>& map) {
+                int res = map.delete_elem(key);
+                if (res < 0 && res != -ENOENT)
+                    ALOGE("Failed to delete map data %s\n", strerror(res.code()));
 
-								// Is empty, and status
-								std::pair<bool, int> isEmpty() const {
-										auto ret = this->getFirstKey();
-										// Return error code ENOENT means the map is empty
-										if (ret.second == -ENOENT)
-												return true;
-										return false;
-								}
+                return res;
+            };
+            iterate(deleteAllEntries);
+            return 0;
+        }
 
-								int iterate(
-												std::function<int(const Key &key, const BpfMap<Key, Value> &map)> &filter) const
-								{
-										std::pair<Key, int> ret = getFirstKey();
-										if (ret.second < 0)
-												return ret.second;
+        // Is empty, and status
+        std::pair<bool, int> isEmpty() const {
+            auto ret = this->getFirstKey();
+            // Return error code ENOENT means the map is empty
+            if (ret.second == -ENOENT)
+                return true;
+            return false;
+        }
 
-										while (ret.second == 0) {
-												int filter_ret = filter(ret.first, *this);
+        int iterate(filter_key_t &filter) const
+        {
+            std::pair<Key, int> ret = getFirstKey();
+            if (ret.second < 0)
+                return ret.second;
 
-												if (filter_ret < 0)
-														return filter_ret;
+            while (ret.second == 0) {
+                int filter_ret = filter(ret.first, *this);
 
-												ret = getNextKey(ret.first);
-										}
+                if (filter_ret < 0)
+                    return filter_ret;
 
-										if (ret == -ENOENT)
-												return 0;
-										else
-												return ret;
-								}
+                ret = getNextKey(ret.first);
+            }
 
-								int iterateWithValue(const std::function<int(const Key &key,
-														const Value &value,
-														const BpfMap<Key, Value> &map)> &filter) const
-								{
-										std::pair<Key, int> ret = getFirstKey();
-										if (ret.second < 0)
-												return ret.second;
+            if (ret == -ENOENT)
+                return 0;
+            else
+                return ret;
+        }
 
-										while (ret.second == 0) {
-												std::pair<Value, int> lookup_ret;
+        int iterateWithValue(filter_key_val_t &filter) const
+        {
+            std::pair<Key, int> ret = getFirstKey();
+            if (ret.second < 0)
+                return ret.second;
 
-												lookup_ret = lookup(ret.first);
-												if (lookup_ret.second < 0)
-														return lookup_ret.second;
+            while (ret.second == 0) {
+                std::pair<Value, int> lookup_ret;
 
-												int filter_ret = filter(ret.first, lookup_ret.first, *this);
-												if (filter_ret < 0)
-														return filter_ret;
+                lookup_ret = lookup(ret.first);
+                if (lookup_ret.second < 0)
+                    return lookup_ret.second;
 
-												ret = getNextKey(ret.first);
-										}
+                int filter_ret = filter(ret.first, lookup_ret.first, *this);
+                if (filter_ret < 0)
+                    return filter_ret;
 
-										if (ret == -ENOENT)
-												return 0;
-										else
-												return ret;
-								}
+                ret = getNextKey(ret.first);
+            }
 
-						private:
-								int mMapFd;
-								std::string mPinnedPath;
-				};
-}  // namespace bpf
-}  // namespace android
+            if (ret == -ENOENT)
+                return 0;
+            else
+                return ret;
+        }
+
+    private:
+        int mMapFd;
+        std::string mPinnedPath;
+};
 
 #endif
+
+/* vim: set ts=4 sw=4 expandtab : */
