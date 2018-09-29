9be819d6a9180d2f4ebe01fbc2ced19f4cae4489
Your Name <you@example.com>
2018-09-29 21:13:50 +0000

    Fix uid_times reading
    Signed-off-by: Your Name <you@example.com>

--- android/tis.cc
+++ android/tis.cc
@@ -6,6 +6,15 @@
 
 using namespace android::bpf;
 
+typedef struct {
+	uint32_t uid;
+	uint32_t freq;
+} time_key_t;
+
+typedef struct {
+	uint64_t ar[100];
+} val_t;
+
 int main(void) {
 	int ret, prog_fd, map_fd;
 
@@ -21,6 +30,20 @@ int main(void) {
 	printf("tp power attached prog %d to %d\n", prog_fd, ret);
 
 	map_fd = bpf_obj_get("/sys/fs/bpf/map_bpf_kern_uid_times");
+	printf("got map %d\n", map_fd);
+
+	sleep(5);
+
+	BpfMap<time_key_t, val_t> m(map_fd);
+        const auto iterf = [](const time_key_t& key,
+			      const val_t& val,
+			      const BpfMap<time_key_t, val_t>& map) {
+		printf("Iter key %u %u, val %d\n", key.uid, key.freq, val.ar[0]);
+		return 0;
+	};
+        m.iterateWithValue(iterf);
+
+
 
 	sleep(3);
 
