d132a0edb73c629ef4b63343f69cf264c75747c3
Your Name <you@example.com>
2018-09-29 00:43:13 +0000

    Add timeinstate building
    Signed-off-by: Your Name <you@example.com>

---
+++ android/tis.cc
@@ -0,0 +1,20 @@
+// Test-program for reading time_in_state
+
+#include "libbpf.h"
+
+using namespace android::bpf;
+
+int main(void) {
+	int ret, prog_fd;
+
+	ret = load_prog("timeinstate/bpf_kern.o");
+	printf("bpf prog loaded... %d\n", ret);
+
+	prog_fd = bpf_obj_get("/sys/fs/bpf/prog_bpf_kern_tracepoint_sched_sched_switch");
+	ret = bpf_attach_tracepoint(prog_fd, "sched", "sched_switch");
+	printf("tp sched sw attach %d\n", ret);
+
+	prog_fd = bpf_obj_get("/sys/fs/bpf/prog_bpf_kern_tracepoint_power_cpu_frequency");
+	ret = bpf_attach_tracepoint(prog_fd, "power", "cpu_frequency");
+	printf("tp power attach %d\n", ret);
+}
